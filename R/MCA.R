e189ec6eb122874d36e834a7f4d48008cceac48f
Cortalak <akira.cortal@gmail.com>
2017-02-13 16:09:22 +0100

    MCA can be done much more quickly by not using ade4 mca but mine. Obtain the same results for both gene and cell and eigen and every other stuff. Also shortened the discrtisation01 method because it was damn stupid.

--- R/MCA.R
+++ R/MCA.R
@@ -46,8 +46,7 @@ Dimension_reduction_MCA <- function(X, Dim = 5) {
     set_colnames(Component) %>% data.frame
 
   X$Dim_Red$Genes_Standard  <- MCA$co %>%
-    set_colnames(Component) %>%
-    as.data.frame
+    set_colnames(Component) %>% data.frame
 
   X$Dim_Red$Genes_Principal <- MCA$c1 %>%
     set_colnames(Component) %>% data.frame
@@ -122,10 +121,10 @@ Dimension_reduction_MCA_FAST <- function(X, Dim = 5) {
   Eig<-Eigen$values[-1]
   Dc<-(1/(sqrt(Acol/sum(MCA))))
   Component <- paste0("Axis", 1:Dim)
-  X$Dim_Red$Cells_Principal<-(sqrt(nrow(MCA))*V)[,1:Dim] %>% set_colnames(Component)%>%  data.frame################l1
-  X$Dim_Red$Cells_Standard<-(sqrt(nrow(MCA))*(V%*%sqrt(D)))[,1:Dim]%>% set_colnames(Component)%>%  data.frame##### li
-  X$Dim_Red$Genes_Standard<-((t(Z)%*%V)*Dc)[,1:Dim] %>% set_colnames(Component)%>%  data.frame
-  X$Dim_Red$Genes_Principal<-sweep(X$Dim_Red$Genes_Standard,2,sqrt(Eig)[2:(Dim+1)],"/")%>%  data.frame
+  X$Dim_Red$Cells_Principal<-(sqrt(nrow(MCA))*V)[,1:Dim] %>% set_colnames(Component)%>% set_rownames(MCA %>% rownames) %>%  data.frame################l1
+  X$Dim_Red$Cells_Standard<-(sqrt(nrow(MCA))*(V%*%sqrt(D)))[,1:Dim]%>% set_colnames(Component)%>% set_rownames(MCA %>% rownames) %>%    data.frame##### li
+  X$Dim_Red$Genes_Standard<-((t(Z)%*%V)*Dc)[,1:Dim] %>% set_colnames(Component)%>% set_rownames(MCA %>% colnames) %>%   data.frame
+  X$Dim_Red$Genes_Principal<-sweep(X$Dim_Red$Genes_Standard,2,sqrt(Eig)[2:(Dim+1)],"/") %>% set_rownames(MCA %>% colnames) %>%  data.frame
   #Distance Calculation
   cat('Calculating Cell to Cell Distance...\n')
   X$Dim_Red$Cell2Cell_Distance <-X$Dim_Red$Cells_Standard[,1:Dim] %>% rdist
@@ -133,7 +132,7 @@ Dimension_reduction_MCA_FAST <- function(X, Dim = 5) {
   X$Dim_Red$Cell2Cell_Distance %<>%  set_rownames(X$Dim_Red$Cells_Principal %>%  rownames)
   X$Dim_Red$Cell2Gene_Distance <- rdist(X$Dim_Red$Cells_Principal, X$Dim_Red$Genes_Standard) %>%  set_colnames(X$Dim_Red$Genes_Standard %>% rownames) %>%  set_rownames(X$Dim_Red$Cells_Principal %>% rownames)
   #End Distance Calculation
-  X$Dim_Red$Eigen_Value <- Eig[Dim] %>% set_names(Component)
+  X$Dim_Red$Eigen_Value <- Eig[1:Dim] %>% set_names(Component)
   EigCum <- ((Eig * 100) / (Eig %>% sum)) %>%  as.data.frame() %>%  mutate(Axis =
                                                                              (paste0("Axis", c(
                                                                                1:length(Eig)
@@ -148,15 +147,13 @@ Dimension_reduction_MCA_FAST <- function(X, Dim = 5) {
   cat('Calculating Distance Wilcoxon...\n')
   Wilcox <- vector(length = (X$Dim_Red$Cells_Standard %>% ncol) - 1)
   for (i in 1:((X$Dim_Red$Cells_Standard %>% ncol) - 1)) {
-    Wil <-
-      wilcox.test(rdist((X$Dim_Red$Cells_Standard)[, 1:i]) %>% as.vector ,
-                  rdist((X$Dim_Red$Cells_Standard)[, 1:(i + 1)]) %>% as.vector,
-                  alternative = "less")
-    Wilcox[i] <- Wil$p.value
+    dis1<-X$Dim_Red$Cells_Standard[, 1:i] %>% rdist %>% as.matrix %>% as.vector
+    dis2<-X$Dim_Red$Cells_Standard[, 1:(i+1)] %>% rdist %>% as.matrix %>% as.vector
+    Wilcox[i] <-wilcox.test(dis1, dis2,alternative = "less") %>%  extract2("p.value")
     names(Wilcox)[i] <- paste0("Axis", i, "-Axis", i + 1)
   }
-  Wilcoxon_df<-tibble(Wilcox %>%  names, Wilcox) %>%  set_colnames(c("Comp","p_value"))
-  X$Dim_Red$Wilcoxon <- Wilcox
+  Wilcoxon_df<-tibble(Wilcox %>%  names, Wilcox) %>%  set_colnames(c("Axis","p_value"))
+  X$Dim_Red$Wilcoxon <- Wilcoxon_df
   #End Calculate Wilcoxon for Cell2CellDistance
 
   #Calculate Correlation Axis and Genes
@@ -169,8 +166,7 @@ Dimension_reduction_MCA_FAST <- function(X, Dim = 5) {
     X$Dim_Red$Cells_Principal %>% ggplot(aes(
       x = Axis1,
       y = Axis2,
-      text = row.names.data.frame(X$Dim_Red$Cells_Principal)
-    )) + geom_point() + theme_light()
+          )) + geom_point() + theme_light()
   X$Dim_Red$Shiny <- X %>% Create_Shiny_Dim_Red
   class(X$Dim_Red) <- "Dim_Red_Object"
   class(X) <- "MCXpress_object"
