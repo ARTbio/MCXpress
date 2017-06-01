4ac50100e6cf79f5ea7e2e4e9c52160591d53f14
Cortalak <akira.cortal@gmail.com>
2017-06-01 10:18:47 +0100

    fixes installation problem

--- R/Data.R
+++ R/Data.R
@@ -1,3 +1,19 @@
+#'Expression Matrix from GEO ID: GSE64553
+#'
+#'The dataset contains only the value for HFF samples
+#'and one outlier has been removed (HFF_PD58_woRotenone2).
+#'
+#'@format A matrix with 47966 genes (row) and 35 individuals (col)
+#'@source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE64553}
+"GSE64553"
+
+#'gmtfile from reactome pathway
+#'
+#'Contains all the geneset of the reactome pathway for human.
+#'@format A list of 1892 reactome pathway geneset with hg19 symbol.
+#'@source \url{http://www.reactome.org/pages/download-data/}
+"reactome_gmtfile"
+
 #' @import magrittr
 #' @import plotly
 #' @import ggplot2
@@ -20,20 +36,3 @@
 #' @importFrom stats cor kmeans na.omit quantile setNames var wilcox.test as.dist cutree hclust
 #' @import lazyeval
 NULL
-
-#'Expression Matrix from GEO ID: GSE64553
-#'
-#'The dataset contains only the value for HFF samples
-#'and one outlier has been removed (HFF_PD58_woRotenone2).
-#'
-#'@format A matrix with 47966 genes (row) and 35 individuals (col)
-#'@source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE64553}
-"GSE64553"
-
-#'gmtfile from reactome pathway
-#'
-#'Contains all the geneset of the reactome pathway for human.
-#'@format A list of 1892 reactome pathway geneset with hg19 symbol.
-#'@source \url{http://www.reactome.org/pages/download-data/}
-"reactome_gmtfile"
-
